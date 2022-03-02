8cfd7e62cccb08f9b50b37610e6b39728c0e00dc
CrazyCloudCraft <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-03-02 10:51:43 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 # Made by CrazyCloudCraft 08.12.2021
 # Update v2.0.1 by CrazyCloudCraft 21.1.2022
-
-# Configuration:
-# Set your server path. Where is your paper.jar?
-MPATH=/opt/Paper
-# What jar and screen name does your Minecraft server have?
-MCNAME=paper
-# Tranzlation:
-MTRANZLATION="The server is not running. Starting server."
-
+# Update v2.3.1 by CrazCloudCraft 02.03.2022
+echo "Restarts?: $TESTMESSAGE"
 
 # Script start. Do not configure this!
 if ! screen -list | grep -q "$MCNAME "; then
