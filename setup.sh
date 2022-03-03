40de349fb51f7a1a6e6a900a6510d5fa2ef0b6e1
CrazyCloudCraft <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-03-03 10:56:22 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Minecraft Server start/stop auto server setup - Setup the server
+# Version 2.3-dev.1 made by CrazyCloudCraft 02.03.2022 https://crazycloudcraft.de 
+
+#install depencies
+if [ $MCSETUP = "TRUE" ]; then
