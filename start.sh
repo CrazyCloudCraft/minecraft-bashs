a3327f1ecd83dd4aa97843654d4d53e94badd2e5
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-03 08:33:35 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -123,7 +123,7 @@ if [ $ASOFTWARE = "PAPER" ]; then
  cd $LPATH/jar
  rm -f version.json
  wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/ -O version.json
- LATEST=`cat version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " "`
+ LATEST=$(cat version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
  wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/builds/$LATEST/downloads/paper-$MAINVERSION-$LATEST.jar
  unzip -qq -t paper-$MAINVERSION-$LATEST.jar
  if [ "$?" -ne 0 ]; then
@@ -148,7 +148,7 @@ if [ $ASOFTWARE = "PURPUR" ]; then
  cd $LPATH/jar
  rm -f version.json
  wget -q https://api.purpurmc.org/v2/purpur/$MAINVERSION -O version.json
- LATEST=`cat version.json | jq -r ".builds" | grep -v "," | grep -v ":" | grep -e "[0-9]" | cut -d "\"" -f2`
+ LATEST=$(cat version.json | jq -r ".builds" | grep -v "," | grep -v ":" | grep -e "[0-9]" | cut -d "\"" -f2)
  wget -q https://api.purpurmc.org/v2/purpur/$MAINVERSION/$LATEST/download -O purpur-$MAINVERSION-$LATEST.jar
  unzip -qq -t purpur-$MAINVERSION-$LATEST.jar
  if [ "$?" -ne 0 ]; then
@@ -195,7 +195,7 @@ if [ $ASOFTWARE = "VELOCITY" ]; then
  cd $LPATH/jar
  rm -f version.json
  wget -q https://papermc.io/api/v2/projects/velocity/versions/$MAINVERSION-SNAPSHOT -O version.json
- LATEST=`cat version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " "`
+ LATEST=$(cat version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
  wget -q https://papermc.io/api/v2/projects/velocity/versions/$MAINVERSION-SNAPSHOT/builds/$LATEST/downloads/velocity-$MAINVERSION-SNAPSHOT-$LATEST.jar
  unzip -qq -t velocity-$MAINVERSION-SNAPSHOT-$LATEST.jar
  if [ "$?" -ne 0 ]; then
@@ -239,53 +239,53 @@ if [ $ASOFTWARE = "BUNGEECORD" ]; then
 fi 
 # Start Bedrock edition part
 #Floodgate: Updateing floodgate for Minecraft PE https://geysermc.org for more information
-if [ UPDATEFLOOD = "TRUE" ]; then
- cd $LPATH/floodgate 
- wget -q https://ci.opencollab.dev//job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar -O floodgate-spigot.jar
- unzip -qq -t floodgate-spigot.jar
- if [ "$?" -ne 0 ]; then
-  echo "Downloaded floodgate-spigot.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
- else
-  diff -q floodgate-spigot.jar ../plugins/floodgate-spigot.jar >/dev/null 2>&1
-  if [ "$?" -eq 1 ]; then
-   /usr/bin/find $LPATH/floodgate/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-   cp floodgate-spigot.jar floodgate-spigot.jar.$(date +%Y.%m.%d.%H.%M.%S)
-   mv floodgate-spigot.jar $LPATH/plugins/floodgate-spigot.jar
-   echo "floodgate-bukkit has been updated" | /usr/bin/logger -t $MCNAME
-  else
-   echo "No floodgate-bukkit update neccessary" | /usr/bin/logger -t $MCNAME
-   rm floodgate-spigot.jar
-  fi
- fi
-fi
+#if [ UPDATEFLOOD = "TRUE" ]; then
+# cd $LPATH/floodgate 
+# wget -q https://ci.opencollab.dev//job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar -O floodgate-spigot.jar
+# unzip -qq -t floodgate-spigot.jar
+# if [ "$?" -ne 0 ]; then
+#  echo "Downloaded floodgate-spigot.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+# else
+#  diff -q floodgate-spigot.jar ../plugins/floodgate-spigot.jar >/dev/null 2>&1
+#  if [ "$?" -eq 1 ]; then
+#   /usr/bin/find $LPATH/floodgate/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+#   cp floodgate-spigot.jar floodgate-spigot.jar.$(date +%Y.%m.%d.%H.%M.%S)
+#   mv floodgate-spigot.jar $LPATH/plugins/floodgate-spigot.jar
+#   echo "floodgate-bukkit has been updated" | /usr/bin/logger -t $MCNAME
+#  else
+#   echo "No floodgate-bukkit update neccessary" | /usr/bin/logger -t $MCNAME
+#   rm floodgate-spigot.jar
+# fi
+# fi
+#fi
 
 #Geyser: Updating Geyser for Minecraft Connections
-if [ UPDATEGEYSER = "TRUE" ]; then
- cd $LPATH/geyser 
- wget -q https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar -O Geyser-Spigot.jar
- unzip -qq -t Geyser-Spigot.jar
- if [ "$?" -ne 0 ]; then
-  echo "Downloaded Geyser-Spigot.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
- else
-  diff -q Geyser-Spigot.jar ../plugins/Geyser-Spigot.jar >/dev/null 2>&1
-  if [ "$?" -eq 1 ]; then
-   /usr/bin/find $LPATH/geyser/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-   cp Geyser-Spigot.jar Geyser-Spigot.jar.$(date +%Y.%m.%d.%H.%M.%S)
-   mv Geyser-Spigot.jar $LPATH/plugins/Geyser-Spigot.jar
-   echo "Geyser-Spigot has been updated" | /usr/bin/logger -t $MCNAME
-  else
-   echo "No Geyser-Spigot update neccessary" | /usr/bin/logger -t $MCNAME
-   rm Geyser-Spigot.jar
-  fi
- fi
-fi
+#if [ UPDATEGEYSER = "TRUE" ]; then
+# cd $LPATH/geyser 
+# wget -q https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar -O Geyser-Spigot.jar
+# unzip -qq -t Geyser-Spigot.jar
+# if [ "$?" -ne 0 ]; then
+#  echo "Downloaded Geyser-Spigot.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+# else
+#  diff -q Geyser-Spigot.jar ../plugins/Geyser-Spigot.jar >/dev/null 2>&1
+#  if [ "$?" -eq 1 ]; then
+#   /usr/bin/find $LPATH/geyser/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+#   cp Geyser-Spigot.jar Geyser-Spigot.jar.$(date +%Y.%m.%d.%H.%M.%S)
+#   mv Geyser-Spigot.jar $LPATH/plugins/Geyser-Spigot.jar
+#   echo "Geyser-Spigot has been updated" | /usr/bin/logger -t $MCNAME
+#  else
+#   echo "No Geyser-Spigot update neccessary" | /usr/bin/logger -t $MCNAME
+#   rm Geyser-Spigot.jar
+#  fi
+# fi
+#fi
 
 #GeyserPROXY: Plugin-Update holen und ggfs installieren
-if [ PRUPDATEGEYSER = "TRUE" ]; then
- cd /opt/Bungee/geyser
- wget -q https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/bungeecord/target/Geyser-BungeeCord.jar -O Geyser-BungeeCord.jar
+#if [ PRUPDATEGEYSER = "TRUE" ]; then
+# cd /opt/Bungee/geyser
+# wget -q https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/bungeecord/target/Geyser-BungeeCord.jar -O Geyser-BungeeCord.jar
  unzip -qq -t Geyser-BungeeCord.jar
- if [ "$?" -ne 0 ]; then
+# if [ "$?" -ne 0 ]; then
   echo "Downloaded Geyser-BungeeCord.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
  else
   diff -q Geyser-BungeeCord.jar ../plugins/Geyser-BungeeCord.jar >/dev/null 2>&1
@@ -296,10 +296,10 @@ if [ PRUPDATEGEYSER = "TRUE" ]; then
    echo "Geyser-BungeeCord has been updated" | /usr/bin/logger -t $MCNAME
   else
    echo "No Geyser-BungeeCord update neccessary" | /usr/bin/logger -t $MCNAME
-   rm Geyser-BungeeCord.jar
-  fi
- fi
-fi
+#   rm Geyser-BungeeCord.jar
+#  fi
+# fi
+#fi
 
 # Stop Bedrock edition part
 
