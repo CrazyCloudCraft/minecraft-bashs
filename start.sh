2330eb3e6b8fcf7f8191f803173cee3888edca26
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-03 10:32:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -119,25 +119,25 @@ fi
 /usr/bin/find $LPATH/logs -type f -mtime +6 -delete > /dev/null 2>&1
 
 #Paper: Getting Update form your selected version.
-if [ $ASOFTWARE = "PAPER" ]; then
- cd $LPATH/jar || exit
+if [ "$ASOFTWARE" = "PAPER" ]; then
+ cd "$LPATH"/jar || exit
  rm -f version.json
- wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/ -O version.json
+ wget -q https://papermc.io/api/v2/projects/paper/versions/"$MAINVERSION"/ -O version.json
  LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
- wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/builds/$LATEST/downloads/paper-$MAINVERSION-$LATEST.jar
- unzip -qq -t paper-$MAINVERSION-$LATEST.jar
+ wget -q https://papermc.io/api/v2/projects/paper/versions/"$MAINVERSION"/builds/"$LATEST"/downloads/paper-"$MAINVERSION"-"$LATEST".jar
+ unzip -qq -t paper-"$MAINVERSION"-"$LATEST".jar
  if [ "$?" -ne 0 ]; then
-  echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+  echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t "$MCNAME"
  else
-  diff -q paper-$MAINVERSION-$LATEST.jar ../$MCNAME.jar >/dev/null 2>&1
+  diff -q paper-"$MAINVERSION"-"$LATEST".jar ../"$MCNAME".jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
-   /usr/bin/find $LPATH/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-   cp paper-$MAINVERSION-$LATEST.jar paper-$MAINVERSION-$LATEST.jar.$(date +%Y.%m.%d.%H.%M.%S)
-   mv paper-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar
-   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
+   /usr/bin/find "$LPATH"/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t "$MCNAME"
+   cp paper-"$MAINVERSION"-"$LATEST".jar paper-"$MAINVERSION"-"$LATEST".jar."$(date +%Y.%m.%d.%H.%M.%S)"
+   mv paper-"$MAINVERSION"-"$LATEST".jar "$LPATH"/"$MCNAME".jar
+   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t "$MCNAME"
   else
-   echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
-   rm paper-$MAINVERSION-$LATEST.jar
+   echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t "$MCNAME"
+   rm paper-"$MAINVERSION"-"$LATEST".jar
    rm version.json
   fi
  fi
