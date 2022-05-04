8645bb363301c8ef0511ccf146db4380d521fd6e
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-04 15:00:06 +0200

    Update bedrock-support.sh

--- test/bedrock-support.sh
+++ test/bedrock-support.sh
@@ -2,7 +2,7 @@
 ASOFTWARE=VELOCITY
 BEDROCKSUPPORT=TRUE
 
-if [ $ASOFTWARE = "PAPER" "SPIGOT" ] && [ $BEDROCKSUPPORT = "TRUE" ]; then
+if [ $ASOFTWARE = "PAPER" or "SPIGOT" ] && [ $BEDROCKSUPPORT = "TRUE" ]; then
 cd "$LPATH"/floodgate || exit && echo "Error #0001"
 wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar 
 fi
