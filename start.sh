c9f22bc5bf255f57696ef9cd624a5fab055f74b2
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-04 15:58:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -128,17 +128,17 @@ if [ $ASOFTWARE = "PAPER" ]; then
  wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/builds/"$LATEST"/downloads/paper-"$MAINVERSION"-"$LATEST".jar
  unzip -qq -t paper-"$MAINVERSION"-"$LATEST".jar
  #if [ "$?" -ne 0 ]; then 
- if ! unzip -qq -t paper-$MAINVERSION-$LATEST.jar; then
-  echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+ if ! unzip -qq -t paper-"$MAINVERSION"-"$LATEST".jar; then
+  echo "Downloaded paper-"$MAINVERSION"-"$LATEST".jar is corrupt. No update." | /usr/bin/logger -t "$MCNAME"
  else
-  diff -q paper-$MAINVERSION-$LATEST.jar ../$MCNAME.jar >/dev/null 2>&1
+  diff -q paper-"$MAINVERSION"-"$LATEST".jar ../"$MCNAME".jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
-   /usr/bin/find $LPATH/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-   cp paper-$MAINVERSION-$LATEST.jar paper-$MAINVERSION-$LATEST.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-   mv paper-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar
-   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
+   /usr/bin/find "$LPATH"/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t "$MCNAME"
+   cp paper-"$MAINVERSION"-"$LATEST".jar paper-"$MAINVERSION"-"$LATEST".jar."$(date +%Y.%m.%d.%H.%M.%S)"
+   mv paper-"$MAINVERSION"-"$LATEST".jar $LPATH/$MCNAME.jar
+   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t "$MCNAME"
   else
-   echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
+   echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t "$MCNAME"
    rm paper-"$MAINVERSION"-"$LATEST".jar
    rm version.json
   fi
