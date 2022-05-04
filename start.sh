9a2caa6cf41cc54a1adf742d6c1dff76b7cdcd7a
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-04 15:22:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -38,13 +38,13 @@ JAVABIN=java17
 # Script start: Do not change after here ###############################################
 
 # Your local absolute path
-LPATH=/"$OPTBASE"/"$SERVERBASE"
-mkdir -p "$LPATH"
+LPATH=/$OPTBASE/$SERVERBASE
+mkdir -p $LPATH
 
 # Install depencies
-touch "$MCNAME".jar
-mkdir -p "$LPATH"/jar
-mkdir -p "$LPATH"/unused 
+touch $MCNAME.jar
+mkdir -p $LPATH/jar
+mkdir -p $LPATH/unused 
 #
  if ! command -v joe &> /dev/null
  then
@@ -103,42 +103,43 @@ if screen -list | grep -q "$MCNAME"; then
 fi
 
 # Change directory to server directory
-cd "$LPATH" || exit
+cd $LPATH || exit
 
 # Create backup for your server
-if [ "$BACKUP" = "TRUE" ]; then
+if [ $BACKUP = "TRUE" ]; then
  if [ -f "$MCNAME.jar" ]; then
     echo "Backing up server (to /$OPTBASE/$BPATH folder)" | /usr/bin/logger -t "$MCNAME"
-    cd /"$OPTBASE"/"$BPATH" && ls -1tr | head -n -10 | xargs -d '\n' rm -f --
-    cd "$LPATH" || exit
-    tar -pzcf ../"$BPATH"/"$(date +%Y.%m.%d.%H.%M.%S)".tar.gz --exclude='FaWeSnapshots/*' --exclude='unused/*' ./
+    cd /$OPTBASE/$BPATH && ls -1tr | head -n -10 | xargs -d '\n' rm -f --
+    cd $LPATH || exit && echo "Error #0002"
+    tar -pzcf ../$BPATH/"$(date +%Y.%m.%d.%H.%M.%S)".tar.gz --exclude='unused/*' ./
  fi
 fi
 
+
 # Clean Logfiles
-/usr/bin/find "$LPATH"/logs -type f -mtime +6 -delete > /dev/null 2>&1
+/usr/bin/find $LPATH/logs -type f -mtime +6 -delete > /dev/null 2>&1
 
 #Paper: Getting Update form your selected version.
-if [ "$ASOFTWARE" = "PAPER" ]; then
- cd "$LPATH"/jar || exit
+if [ $ASOFTWARE = "PAPER" ]; then
+ cd $LPATH/jar || exit
  rm -f version.json
- wget -q https://papermc.io/api/v2/projects/paper/versions/"$MAINVERSION"/ -O version.json
+ wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/ -O version.json
  LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
- wget -q https://papermc.io/api/v2/projects/paper/versions/"$MAINVERSION"/builds/"$LATEST"/downloads/paper-"$MAINVERSION"-"$LATEST".jar
- unzip -qq -t paper-"$MAINVERSION"-"$LATEST".jar
+ wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/builds/$LATEST/downloads/paper-$MAINVERSION-$LATEST.jar
+ unzip -qq -t paper-$MAINVERSION-$LATEST.jar
  #if [ "$?" -ne 0 ]; then 
- if ! unzip -qq -t paper-"$MAINVERSION"-"$LATEST".jar; then
-  echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t "$MCNAME"
+ if ! unzip -qq -t paper-$MAINVERSION-$LATEST.jar; then
+  echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
  else
-  diff -q paper-"$MAINVERSION"-"$LATEST".jar ../"$MCNAME".jar >/dev/null 2>&1
+  diff -q paper-$MAINVERSION-$LATEST.jar ../$MCNAME.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
-   /usr/bin/find "$LPATH"/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t "$MCNAME"
-   cp paper-"$MAINVERSION"-"$LATEST".jar paper-"$MAINVERSION"-"$LATEST".jar."$(date +%Y.%m.%d.%H.%M.%S)"
-   mv paper-"$MAINVERSION"-"$LATEST".jar "$LPATH"/"$MCNAME".jar
-   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t "$MCNAME"
+   /usr/bin/find $LPATH/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   cp paper-$MAINVERSION-$LATEST.jar paper-$MAINVERSION-$LATEST.jar."$(date +%Y.%m.%d.%H.%M.%S)"
+   mv paper-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar
+   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
   else
-   echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t "$MCNAME"
-   rm paper-"$MAINVERSION"-"$LATEST".jar
+   echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
+   rm paper-$MAINVERSION-$LATEST.jar
    rm version.json
   fi
  fi
