7b0c5f1aa5b62b7c3ca6d51e6d8c4f89ed4f5df6
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-05 09:09:28 +0200

    Update bedrock-support.sh

--- test/bedrock-support.sh
+++ test/bedrock-support.sh
@@ -4,21 +4,21 @@ BESUPPORT=TRUE
 LPATH=Coding
 
 if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] && [ $BESUPPORT = "TRUE" ]; then
- cd $LPATH/floodgate || exit
+ cd $LP
+ cd $LPATH/mcsys/floodgate || exit
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar
  unzip -qq -t floodgate-spigot.jar
  if ! unzip -qq -t floodgate-spigot.jar; then
-  echo "Downloaded floodgate-spigot.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+  echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
  else
-  touch ../plugins/floodgate-spigot.jar
-  diff -q floodgate-spigot.jar ../plugins/floodgate-spigot.jar >/dev/null 2>&1
+  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
-  /usr/bin/find $LPATH/floodgate/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+  /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   cp floodgate-spigot.jar floodgate-spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-  mv floodgate-spigot.jar $LPATH/plugins/floodgate-spigot.jar
-   echo "floodgate-bukkit has been updated" | /usr/bin/logger -t $MCNAME
+  mv floodgate-spigot.jar ../../plugins/floodgate-spigot.jar
+   echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
   else
-   echo "No floodgate-bukkit update neccessary" | /usr/bin/logger -t $MCNAME
+   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
    rm floodgate-spigot.jar
   fi
  fi
