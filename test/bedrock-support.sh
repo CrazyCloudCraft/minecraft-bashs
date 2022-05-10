8478f23c46edca493f71a948fd229855e54e1323
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-10 11:15:29 +0200

    Update bedrock-support.sh

--- test/bedrock-support.sh
+++ test/bedrock-support.sh
@@ -39,14 +39,14 @@ if [ $ASOFTWARE = "BUNGEECORD" ] || [ $ASOFTWARE = "VELOCITY" ] || [ && [ $BESUP
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/bungee/target/floodgate-bungee.jar
  unzip -qq -t floodgate-bungee.jar
  if ! unzip -qq -t floodgate-bungee.jar; then
-  echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
+  echo "Downloaded floodgate for Bungeecord and Waterfall is corrupt. No update." | /usr/bin/logger -t $MCNAME
 # else
 #  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
   /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   cp floodgate-bungee.jar floodgate-bungee.jar."$(date +%Y.%m.%d.%H.%M.%S)"
   mv floodgate-bungee.jar ../../plugins/floodgate-bungee.jar
-   echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
+   echo "floodgate for Bungeecord and Waterfall has been updated" | /usr/bin/logger -t $MCNAME
 #  else
 #   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
 #   rm floodgate-bungee.jar
@@ -54,14 +54,14 @@ if [ $ASOFTWARE = "BUNGEECORD" ] || [ $ASOFTWARE = "VELOCITY" ] || [ && [ $BESUP
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/velocity/target/floodgate-velocity.jar
  unzip -qq -t floodgate-velocity.jar
  if ! unzip -qq -t floodgate-velocity.jar; then
-  echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
+  echo "Downloaded floodgate for velocity is corrupt. No update." | /usr/bin/logger -t $MCNAME
 # else
 #  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
   /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   cp floodgate-velocity.jar floodgate-velocity.jar."$(date +%Y.%m.%d.%H.%M.%S)"
   mv floodgate-velocity.jar ../../plugins/floodgate-velocity.jar
-   echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
+   echo "floodgate for velocity has been updated" | /usr/bin/logger -t $MCNAME
 #  else
 #   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
 #   rm floodgate-bungee.jar
@@ -78,14 +78,14 @@ if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "PURP
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
  unzip -qq -t Geyser-Spigot.jar
  if ! unzip -qq -t Geyser-Spigot.jar; then
-  echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
+  echo "Downloaded Geyser Default is corrupt. No update." | /usr/bin/logger -t $MCNAME
 # else
 #  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
   /usr/bin/find $LPATH/mcsys/geyser/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   cp Geyser-Spigot.jar Geyser-Spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
   mv Geyser-Spigot.jar ../../plugins/Geyser-Spigot.jar
-   echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
+   echo "Geyser Default has been updated" | /usr/bin/logger -t $MCNAME
 #  else
 #   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
 #   rm floodgate-spigot.jar
@@ -102,14 +102,14 @@ if [ $ASOFTWARE = "BUNGEECORD" ] || [ $ASOFTWARE = "VELOCITY" ] || [ && [ $BESUP
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/bungeecord/target/Geyser-BungeeCord.jar
  unzip -qq -t Geyser-BungeeCord.jar
  if ! unzip -qq -t Geyser-BungeeCord.jar; then
-  echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
+  echo "Downloaded Geyser Bungeecord and Waterfall is corrupt. No update." | /usr/bin/logger -t $MCNAME
 # else
 #  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
   /usr/bin/find $LPATH/mcsys/geyser/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   cp Geyser-BungeeCord.jar Geyser-BungeeCord.jar."$(date +%Y.%m.%d.%H.%M.%S)"
   mv Geyser-BungeeCord.jar ../../plugins/Geyser-BungeeCord.jar
-   echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
+   echo "Geyser for Bungeecord and Waterfall has been updated" | /usr/bin/logger -t $MCNAME
 #  else
 #   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
 #   rm floodgate-bungee.jar
