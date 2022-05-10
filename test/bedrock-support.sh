7b7ee6e0b1f475a044e7130069601c447dd71cda
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-10 11:16:05 +0200

    Update bedrock-support.sh

--- test/bedrock-support.sh
+++ test/bedrock-support.sh
@@ -15,14 +15,14 @@ if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "PURP
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar
  unzip -qq -t floodgate-spigot.jar
  if ! unzip -qq -t floodgate-spigot.jar; then
-  echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
+  echo "Downloaded floodgate default is corrupt. No update." | /usr/bin/logger -t $MCNAME
 # else
 #  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
   /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   cp floodgate-spigot.jar floodgate-spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
   mv floodgate-spigot.jar ../../plugins/floodgate-spigot.jar
-   echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
+   echo "floodgate default has been updated" | /usr/bin/logger -t $MCNAME
 #  else
 #   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
 #   rm floodgate-spigot.jar
