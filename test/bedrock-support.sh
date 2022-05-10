7a5a5a0790da6db0cab74bcc68525196867a3aab
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-10 10:59:45 +0200

    Update bedrock-support.sh

--- test/bedrock-support.sh
+++ test/bedrock-support.sh
@@ -72,8 +72,8 @@ fi
 if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "PURPUR" ] && [ $GBESUPPORT = "TRUE" ]; then
  cd $LPATH/plugins || exit
 # touch Geyser-Spigot.jar
- mkdir -p $LPATH/mcsys/floodgate
- cd $LPATH/mcsys/floodgate || exit
+ mkdir -p $LPATH/mcsys/geyser
+ cd $LPATH/mcsys/geyser || exit
  wget -q https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
  unzip -qq -t Geyser-Spigot.jar
  if ! unzip -qq -t Geyser-Spigot.jar; then
@@ -81,9 +81,9 @@ if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "PURP
 # else
 #  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
-  /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-  cp floodgate-spigot.jar floodgate-spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-  mv floodgate-spigot.jar ../../plugins/floodgate-spigot.jar
+  /usr/bin/find $LPATH/mcsys/geyser/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+  cp Geyser-Spigot.jar Geyser-Spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
+  mv Geyser-Spigot.jar ../../plugins/Geyser-Spigot.jar
    echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
 #  else
 #   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
