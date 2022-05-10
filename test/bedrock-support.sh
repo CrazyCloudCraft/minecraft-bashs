dbea830b9d14317e233945f0b873a2602baf4f81
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-10 10:49:39 +0200

    Update bedrock-support.sh

--- test/bedrock-support.sh
+++ test/bedrock-support.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 ASOFTWARE=SPIGOT
 BESUPPORT=TRUE
+GBESUPPORT=TRUE
 LPATH=Coding
 mkdir -p $LPATH/plugins
 mkdir -p $LPATH/mcsys
+
 # Floodgate for Spigot
 if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "PURPUR" ] && [ $BESUPPORT = "TRUE" ]; then
  cd $LPATH/plugins || exit
- touch floodgate-spigot.jar
+# touch floodgate-spigot.jar
  mkdir -p $LPATH/mcsys/floodgate
  cd $LPATH/mcsys/floodgate || exit
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar
@@ -63,6 +65,29 @@ if [ $ASOFTWARE = "BUNGEECORD" ] || [ $ASOFTWARE = "VELOCITY" ] || [ && [ $BESUP
 #  else
 #   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
 #   rm floodgate-bungee.jar
+  fi
+ fi
+fi
+# Geyser Updater
+if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "PURPUR" ] && [ $GBESUPPORT = "TRUE" ]; then
+ cd $LPATH/plugins || exit
+# touch Geyser-Spigot.jar
+ mkdir -p $LPATH/mcsys/floodgate
+ cd $LPATH/mcsys/floodgate || exit
+ wget -q https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
+ unzip -qq -t floodgate-spigot.jar
+ if ! unzip -qq -t floodgate-spigot.jar; then
+  echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
+# else
+#  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
+  if [ "$?" -eq 1 ]; then
+  /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+  cp floodgate-spigot.jar floodgate-spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
+  mv floodgate-spigot.jar ../../plugins/floodgate-spigot.jar
+   echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
+#  else
+#   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
+#   rm floodgate-spigot.jar
   fi
  fi
 fi
