3413194b5596a2f8f45b9afed56e259efdabffb9
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-10 11:07:04 +0200

    Update bedrock-support.sh

--- test/bedrock-support.sh
+++ test/bedrock-support.sh
@@ -75,7 +75,7 @@ if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "PURP
 # touch Geyser-Spigot.jar
  mkdir -p $LPATH/mcsys/geyser
  cd $LPATH/mcsys/geyser || exit
- wget -q https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
+ wget -q https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
  unzip -qq -t Geyser-Spigot.jar
  if ! unzip -qq -t Geyser-Spigot.jar; then
   echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
@@ -93,3 +93,41 @@ if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "PURP
  fi
 fi
 
+# Geyser for Proxy
+if [ $ASOFTWARE = "BUNGEECORD" ] || [ $ASOFTWARE = "VELOCITY" ] || [ && [ $BESUPPORT = "TRUE" ]; then
+ cd $LPATH/plugins || exit
+ mkdir -p $LPATH/mcsys/geyser
+ cd $LPATH/mcsys/geyser || exit
+ if [ $ASOFTWARE = "BUNGEECORD" ] || [ $ASOFTWARE = "WATERFALL" ]; then
+ wget -q https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/bungeecord/target/Geyser-BungeeCord.jar
+ unzip -qq -t Geyser-BungeeCord.jar
+ if ! unzip -qq -t Geyser-BungeeCord.jar; then
+  echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
+# else
+#  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
+  if [ "$?" -eq 1 ]; then
+  /usr/bin/find $LPATH/mcsys/geyser/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+  cp Geyser-BungeeCord.jar Geyser-BungeeCord.jar."$(date +%Y.%m.%d.%H.%M.%S)"
+  mv Geyser-BungeeCord.jar ../../plugins/Geyser-BungeeCord.jar
+   echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
+#  else
+#   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
+#   rm floodgate-bungee.jar
+ if [ $ASOFTWARE = "VELOCITY" ]; then
+ wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/velocity/target/floodgate-velocity.jar
+ unzip -qq -t floodgate-velocity.jar
+ if ! unzip -qq -t floodgate-velocity.jar; then
+  echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
+# else
+#  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
+  if [ "$?" -eq 1 ]; then
+  /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+  cp floodgate-velocity.jar floodgate-velocity.jar."$(date +%Y.%m.%d.%H.%M.%S)"
+  mv floodgate-velocity.jar ../../plugins/floodgate-velocity.jar
+   echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
+#  else
+#   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
+#   rm floodgate-bungee.jar
+  fi
+ fi
+fi
