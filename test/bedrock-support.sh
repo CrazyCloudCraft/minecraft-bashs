0240950df0d6d8232d4e561a376bab856c8f4171
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-10 10:37:57 +0200

    Update bedrock-support.sh

--- test/bedrock-support.sh
+++ test/bedrock-support.sh
@@ -13,16 +13,16 @@ if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "PURP
  unzip -qq -t floodgate-spigot.jar
  if ! unzip -qq -t floodgate-spigot.jar; then
   echo "Downloaded floodgate is corrupt. No update." | /usr/bin/logger -t $MCNAME
- else
-  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
+# else
+#  diff -q floodgate-spigot.jar ../../plugins/floodgate-spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
   /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   cp floodgate-spigot.jar floodgate-spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
   mv floodgate-spigot.jar ../../plugins/floodgate-spigot.jar
    echo "floodgate has been updated" | /usr/bin/logger -t $MCNAME
-  else
-   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
-   rm floodgate-spigot.jar
+#  else
+#   echo "No floodgate update neccessary" | /usr/bin/logger -t $MCNAME
+#   rm floodgate-spigot.jar
   fi
  fi
 fi
