04383360867c6113fd1d2a735cb768ddab1b7e68
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-12 07:21:50 +0000

    Update spigot-updater.sh

--- test/spigot-updater.sh
+++ test/spigot-updater.sh
@@ -12,15 +12,17 @@ if [ $ASOFTWARE = "SPIGOT" ]; then
  else
   diff -q BuildTools.jar ../spitool/BuildTools.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
-   /usr/bin/find $LPATH/mcsys/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   /usr/bin/find $LPATH/mcsys/build/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   /usr/bin/find $LPATH/mcsys/spitool/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
    cd ../spitool/ || exit
    mv BuildTools.jar BuildTools.jar"$(date +%Y.%m.%d.%H.%M.%S)"
    cd ../build || exit
    cp BuildTools.jar ../spitool/BuildTools.jar
    java -jar BuildTools.jar --rev $MAINVERSION
    cp ./BuildTools/spigot-$MAINVERSION.jar ./spigot-$MAINVERSION.jar"$(date +%Y.%m.%d.%H.%M.%S)" $LPATH/$MCNAME.jar
-   mv spigot.jar $LPATH/$MCNAME.jar
-   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
+   mv spigot-$MAINVERSION.jar $LPATH/$MCNAME.jar
+   rm -r BuildTools
+   echo "spigot-$MAINVERSION.jar has been updated" | /usr/bin/logger -t $MCNAME
   else
    echo "No BuildTools.jar update neccessary" | /usr/bin/logger -t $MCNAME
    rm BuildTools.jar
