9f5a78a8ce04c9a21c98c8e37529293ce77eacae
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-14 12:47:11 +0200

    Update server_installer-v1.0.sh

--- .github/server_installer-v1.0.sh
+++ .github/server_installer-v1.0.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Minecraft Server installer for Easy Setup
 # Made By CrazyCloudCraft - Argantiu GmbH
+MCINSTALLERV=v1.0
 MCSETUPVERSION=v2.5.1.1
 wget -q https://github.com/CrazyCloudCraft/minecraft-bashs/releases/download/"$MCSETUPVERSION"/server-start-scripts-"$MCSETUPVERSION".zip 
 apt-get -qq install zip
@@ -13,5 +14,5 @@ rm server-start-scripts-"$MCSETUPVERSION".zip
 echo -e "\033[1;32m__________"
 echo -e "\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m Setup finished!"
 echo -e "\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m Please edit now the .sh files with joe <file>.sh"
-rm server_installer-v1.0.sh
+rm server_installer-"$MCINSTALLERV".sh
 exit 1
