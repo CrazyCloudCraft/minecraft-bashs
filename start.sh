d7c777f19af3f025e1e5bc0f7c629cf7847b9939
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-24 17:34:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -164,3 +164,104 @@ if [ $ASOFTWARE = "PURPUR" ]; then
   fi
  fi
 fi
+#Mohist: Getting Update form your selected version.
+if [ $ASOFTWARE = "MOHIST" ]; then
+ cd $LPATH/jar
+ DATE=$(date +%Y.%m.%d.%H.%M.%S)
+ wget -q https://mohistmc.com/api/$MAINVERSION/latest/download -O mohist-$MAINVERSION-$DATE.jar
+ unzip -qq -t  mohist-$MAINVERSION-$DATE.jar
+ if [ "$?" -ne 0 ]; then
+  echo "Downloaded mohist-$MAINVERSION-$DATE.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+ else
+  diff -q mohist-$MAINVERSION-$DATE.jar ../$MCNAME.jar >/dev/null 2>&1
+  if [ "$?" -eq 1 ]; then
+   /usr/bin/find $LPATH/jar/* -type f -mtime +30 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   cp mohist-$MAINVERSION-$DATE.jar  mohist-$MAINVERSION-$DATE.jar.backup
+   mv mohist-$MAINVERSION-$DATE.jar $LPATH/$MCNAME.jar
+   echo "mohist-$MAINVERSION-$DATE.jar has been updated" | /usr/bin/logger -t $MCNAME
+  else
+   echo "No mohist-$MAINVERSION-$DATE.jar update neccessary" | /usr/bin/logger -t $MCNAME
+   rm mohist-$MAINVERSION-$DATE.jar
+  fi
+ fi
+fi
+
+#Velocity: Getting Update form your selected version.
+if [ $ASOFTWARE = "VELOCITY" ]; then
+ cd $LPATH/jar
+ rm -f version.json
+ wget -q https://papermc.io/api/v2/projects/velocity/versions/$MAINVERSION-SNAPSHOT -O version.json
+ LATEST=`cat version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " "`
+ wget -q https://papermc.io/api/v2/projects/velocity/versions/$MAINVERSION-SNAPSHOT/builds/$LATEST/downloads/velocity-$MAINVERSION-SNAPSHOT-$LATEST.jar
+ unzip -qq -t velocity-$MAINVERSION-SNAPSHOT-$LATEST.jar
+ if [ "$?" -ne 0 ]; then
+  echo "Downloaded velocity-$MAINVERSION-SNAPSHOT-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+ else
+  diff -q velocity-$MAINVERSION-SNAPSHOT-$LATEST.jar ../$MCNAME.jar >/dev/null 2>&1
+  if [ "$?" -eq 1 ]; then
+   /usr/bin/find $LPATH/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   cp velocity-$MAINVERSION-SNAPSHOT-$LATEST.jar velocity-$MAINVERSION-SNAPSHOT-$LATEST.jar.$(date +%Y.%m.%d.%H.%M.%S)
+   mv velocity-$MAINVERSION-SNAPSHOT-$LATEST.jar $LPATH/$MCNAME.jar
+   echo "velocity-$MAINVERSION-SNAPSHOT-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
+  else
+   echo "No velocity-$MAINVERSION-SNAPSHOT-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
+   rm velocity-$MAINVERSION-SNAPSHOT-$LATEST.jar
+   rm version.json
+  fi
+ fi
+fi
+
+#Bungeecord: Getting Update form your selected version.
+if [ $ASOFTWARE = "BUNGEECORD" ]; then
+ cd $LPATH/jar
+ rm -f version.json
+ wget -q https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
+ unzip -qq -t BungeeCord.jar
+ if [ "$?" -ne 0 ]; then
+  echo "Downloaded BungeeCord.jar No update." | /usr/bin/logger -t $MCNAME
+ else
+  diff -q BungeeCord.jar ../$MCNAME.jar >/dev/null 2>&1
+  if [ "$?" -eq 1 ]; then
+   /usr/bin/find $LPATH/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   cp BungeeCord.jar BungeeCord.jar.$(date +%Y.%m.%d.%H.%M.%S)
+   mv BungeeCord.jar $LPATH/$MCNAME.jar
+   echo "BungeeCord.jar has been updated" | /usr/bin/logger -t $MCNAME
+  else
+   echo "No BungeeCord.jar update neccessary" | /usr/bin/logger -t $MCNAME
+   rm BungeeCord.jar
+   rm -f version.json
+  fi
+ fi
+fi 
+# Bedrock edition
+
+mkdir -p $LPATH/plugins
+mkdir -p $LPATH/mcsys
+
+# Floodgate for Spigot
+if [ $ASOFTWARE = "PAPER" ] || [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "BUKKIT" ] || [ $ASOFTWARE = "PURPUR" ] && [ $BESUPPORT = "TRUE" ]; then
+ cd $LPATH/plugins || exit 1
+ mkdir -p $LPATH/mcsys/floodgate
+ cd $LPATH/mcsys/floodgate || exit 1
+ wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar -O floodgate-spigot.jar
+ unzip -qq -t floodgate-spigot.jar
+ if [ "$?" -ne 0 ]; then
+  echo "Downloaded floodgate default is corrupt. No update." | /usr/bin/logger -t $MCNAME
+ else
+  if [ -f $LPATH/plugins/floodgate-spigot.jar ]; then
+   echo "Floodgate plugin exists" | /usr/bin/logger -t $MCNAME
+  else
+   touch $LPATH/plugins/floodgate-spigot.jar 
+  fi
+  diff -q floodgate-spigot.jar $LPATH/plugins/floodgate-spigot.jar >/dev/null 2>&1
+  if [ "$?" -eq 1 ]; then
+   cp floodgate-spigot.jar floodgate-spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
+   mv floodgate-spigot.jar $LPATH/plugins/floodgate-spigot.jar
+   /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   echo "floodgate default has been updated" | /usr/bin/logger -t $MCNAME
+  else
+   echo "No floodgate default update neccessary" | /usr/bin/logger -t $MCNAME
+   rm floodgate-spigot.jar
+  fi
+ fi
+fi
