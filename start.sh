7c50279c5714af712bc126123ea85d8e8ca3fdf4
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-24 17:31:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -116,3 +116,51 @@ fi
 /usr/bin/find $LPATH/logs -type f -mtime +6 -delete > /dev/null 2>&1
 
 #Paper: Getting Update form your selected version.
+if [ $ASOFTWARE = "PAPER" ]; then
+ cd $LPATH/jar
+ rm -f version.json
+ wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/ -O version.json
+ LATEST=`cat version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " "`
+ wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/builds/$LATEST/downloads/$MCNAME-$MAINVERSION-$LATEST.jar
+ unzip -qq -t paper-$MAINVERSION-$LATEST.jar
+ if [ "$?" -ne 0 ]; then
+  echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+ else
+  diff -q paper-$MAINVERSION-$LATEST.jar ../$MCNAME.jar >/dev/null 2>&1
+  if [ "$?" -eq 1 ]; then
+   /usr/bin/find $LPATH/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   cp paper-$MAINVERSION-$LATEST.jar paper-$MAINVERSION-$LATEST.jar.$(date +%Y.%m.%d.%H.%M.%S)
+   mv paper-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar
+   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
+  else
+   echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
+   rm paper-$MAINVERSION-$LATEST.jar
+   rm version.json
+  fi
+ fi
+fi
+
+#PurPur: Getting Update form your selected version.
+if [ $ASOFTWARE = "PURPUR" ]; then
+ cd $LPATH/jar
+ rm -f version.json
+ wget -q https://api.purpurmc.org/v2/purpur/$MAINVERSION -O version.json
+ LATEST=`cat version.json | jq -r ".builds" | grep -v "," | grep -v ":" | grep -e "[0-9]" | cut -d "\"" -f2`
+ wget -q https://api.purpurmc.org/v2/purpur/$MAINVERSION/$LATEST/download -O purpur-$MAINVERSION-$LATEST.jar
+ unzip -qq -t purpur-$MAINVERSION-$LATEST.jar
+ if [ "$?" -ne 0 ]; then
+  echo "Downloaded purpur-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+ else
+  diff -q purpur-$MAINVERSION-$LATEST.jar ../$MCNAME.jar >/dev/null 2>&1 
+  if [ "$?" -eq 1 ]; then
+   /usr/bin/find $LPATH/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   cp purpur-$MAINVERSION-$LATEST.jar purpur-$MAINVERSION-$LATEST.jar.$(date +%Y.%m.%d.%H.%M.%S)
+   mv purpur-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar
+   echo "purpur-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
+  else
+   echo "No purpur-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
+   rm purpur-$MAINVERSION-$LATEST.jar
+   rm version.json
+  fi
+ fi
+fi
