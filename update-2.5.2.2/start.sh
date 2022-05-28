f06e3e83d0db1fabb23dc412d3a45b46ff6d7468
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-28 22:18:05 +0200

    Update start.sh

--- update-2.5.2.2/start.sh
+++ update-2.5.2.2/start.sh
@@ -9,6 +9,9 @@
 # Define your Minecraft version like 1.18.2 based on your Software
 # For Velocity you set the latest PROXY version ( in the moment: 3.1.2 (just look on https://papermc.io/downloads#Velocity )
 MAINVERSION=1.18.2
+
+# Velocity Version. Please only change, if there is a new version.
+PRMCVERSION=3.1.2
 # What type of Server Software do you use?
 # You can use: PAPER, PURPUR, MOHIST, VELOCITY, BUNGEECORD
 ASOFTWARE=PAPER
@@ -228,3 +231,30 @@ if [ $ASOFTWARE = "SPIGOT" ] || [ $ASOFTWARE = "BUKKIT" ]; then
 fi
 
 #Velocity: Getting Update form your selected version.
+if [ $ASOFTWARE = "VELOCITY" ]; then
+ mkdir -p $LPATH/mcsys/jar
+ cd $LPATH/mcsys/jar || exit 1
+ rm -f version.json
+ wget -q https://api.papermc.io/v2/projects/velocity/versions/$PRMCVERSION-SNAPSHOT -O version.json
+ LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
+ wget -q https://api.papermc.io/v2/projects/velocity/versions/$PRMCVERSION-SNAPSHOT/builds/$LATEST/downloads/velocity-$PRMCVERSION-SNAPSHOT-$LATEST.jar -O velocity-$PRMCVERSION-SNAPSHOT-$LAT
+ unzip -qq -t velocity-$PRMCVERSION-SNAPSHOT-$LATEST.jar
+ if [ "$?" -ne 0 ]; then
+  echo "Downloaded velocity-$PRMCVERSION-SNAPSHOT-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+ else
+  diff -q velocity-$PRMCVERSION-SNAPSHOT-$LATEST.jar $LPATH/$MCNAME.jar >/dev/null 2>&1
+  if [ "$?" -eq 1 ]; then
+   cp velocity-$PRMCVERSION-SNAPSHOT-$LATEST.jar velocity-$PRMCVERSION-SNAPSHOT-$LATEST.jar."$(date +%Y.%m.%d.%H.%M.%S)"
+   mv velocity-$PRMCVERSION-SNAPSHOT-$LATEST.jar $LPATH/$MCNAME.jar
+   /usr/bin/find $LPATH/mcsys/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   echo "velocity-$PRMCVERSION-SNAPSHOT-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
+   rm version.json
+  else
+   echo "No velocity-$PRMCVERSION-SNAPSHOT-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
+   rm velocity-$PRMCVERSION-SNAPSHOT-$LATEST.jar
+   rm version.json
+  fi
+ fi
+fi
+
+#Bungeecord: Getting Update form your selected version.
