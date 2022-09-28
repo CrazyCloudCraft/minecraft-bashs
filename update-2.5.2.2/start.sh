b9397aea333652756813dcd2609d9bcc940d6af7
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-09-28 16:42:19 +0200

    Update start.sh

--- update-2.5.2.2/start.sh
+++ update-2.5.2.2/start.sh
@@ -151,7 +151,7 @@ if [ $ASOFTWARE = "PURPUR" ]; then
  cd $LPATH/mcsys/jar || exit 1
  rm -f version.json
  wget -q https://api.purpurmc.org/v2/purpur/$MAINVERSION -O version.json
- LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -v ":" | grep -e "[0-9]" | cut -d "\"" -f2)
+ LATEST=cat < version.json | jq -r ".builds" | grep -v "," | grep -v ":" | grep -e "[0-9]" | cut -d "\"" -f2
  wget -q https://api.purpurmc.org/v2/purpur/$MAINVERSION/$LATEST/download -O purpur-$MAINVERSION-$LATEST.jar
  unzip -qq -t purpur-$MAINVERSION-$LATEST.jar
  if [ "$?" -ne 0 ]; then
