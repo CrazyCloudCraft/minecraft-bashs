95ef479b507e484c0688d3ed5d1925cc5af0fc33
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-11-17 08:50:47 +0100

    Create start.sh

---
+++ update-2.6.0.0/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Minecraft manager by CrazyCloudCraft https://crazycloudcraft.de
+# This is a rewrite of the old version but only with old functions
+
+AVERSION=
+AVELOVERSION=
+ASOFTWARE=
+OPTBASE=
